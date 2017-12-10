5765f028af2f69c5dc8b95845f504d1378040110
Unknown <sabri.elgueder@satoripop.com>
2017-12-10 16:57:35 +0100

    add insertBefore

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = function prependReactDOM(Component, el, props, done) {
 	el.forEach(function (dom) {
 		let div = document.createElement('div');
 		let r = ReactDOM.render(React.createElement(Component, props, null), div, function () {
-			dom.append(ReactDOM.findDOMNode(this));
+			dom.insertBefore(ReactDOM.findDOMNode(this));
 			typeof done === 'function' && done();
 		});
 	});
